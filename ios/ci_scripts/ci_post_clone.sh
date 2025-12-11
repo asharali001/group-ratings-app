6f272735448f3b6e3bd316e247f73a5429c153c7
asharali001 <asharalijamshaid@gmail.com>
2025-12-11 17:44:58 +0200

    Add Xcode Cloud Flutter setup script

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Fail on any error
+set -e
+
+# The default location for the repo in Xcode Cloud is $CI_PRIMARY_REPOSITORY_PATH
+echo "📍 Navigate to project root"
+cd $CI_PRIMARY_REPOSITORY_PATH
+
+echo "⬇️ Install Flutter"
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$HOME/flutter/bin:$PATH"
+
+echo "✅ Flutter Version:"
+flutter --version
+
+echo "📦 Install Flutter Dependencies"
+flutter pub get
+
+echo "🧱 Install CocoaPods"
+# Note: CocoaPods is pre-installed in Xcode Cloud, but we need to run it to generate the Pods project linking to Flutter.
+cd ios
+pod install
+
+echo "✨ flutter build ios --config-only (to generate xcconfig)"
+# This ensures Generated.xcconfig exists
+cd ..
+flutter build ios --config-only --no-codesign
+
+echo "✅ Setup Complete"
